7e48e667fada08e4db42955db3eb07821b28702a
Hussain Almarzooq <hhaalmarzouq@gmail.com>
2023-02-13 21:43:37 +0300

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import{BasicHighlightDirecteive} from'./basic-highlight/basic-highlight.directive';
-import { HighlightDirective } from './highlight.directive';
+import { HighlightDirective } from './basic-highlight/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirecteive,
+    
     HighlightDirective
   ],
   imports: [
@@ -17,4 +16,5 @@ import { HighlightDirective } from './highlight.directive';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/basic-highlight/highlight.directive.ts
+++ src/app/basic-highlight/highlight.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef,OnInit } from '@angular/core';
+import {  Directive, ElementRef,OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
   constructor(private elmentRef:ElementRef){
-  }
+      this.elmentRef.nativeElement.style.backgroundColor = 'yellow';
+    }
+
+    
   ngOnInit(): void {
     throw new Error("Method not implemented.");
-    this.elmentRef.nativeElement.style.backgroundColor='green';
+
   }
   
 }
